6269acf30d112a35e06ae3c3d3e9888dd670a6ce
Nikhil Kishan Khaneja <nkhaneja09@gmail.com>
2020-06-24 22:01:11 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn my_personal_portfolio.wsgi:application --log-file -
---
+++ my_personal_portfolio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MyPersonalPortfolioConfig(AppConfig):
+    name = 'my_personal_portfolio'
